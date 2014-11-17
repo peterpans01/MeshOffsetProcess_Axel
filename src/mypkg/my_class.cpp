12c3f7b4d617ced2178ed19457bbcbdc25f3bb41
peterpans01 <peterpans2014@gmail.com>
2014-11-17 10:44:23 +0100

    add source codes, CMake,...

---
+++ src/mypkg/my_class.cpp
@@ -0,0 +1,8 @@
+#include <mypkg/my_class.h>
+
+/** Default constructor.
+    It just outputs a message.
+**/
+my_class::my_class() {
+  std::cout << "Constructor of my_class"<<std::endl;
+}
---
+++ src/mypkg/my_class.h
@@ -0,0 +1,15 @@
+#ifndef MY_CLASS_HPP
+#define MY_CLASS_HPP
+#include <iostream>
+
+
+/** A very simple class.
+
+**/
+struct my_class {
+
+  my_class();
+
+};
+
+#endif
